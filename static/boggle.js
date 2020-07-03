a2f30c00753500261557cab5d161ea305990f922
Justin Vendettuoli <jvendettuoli@gmail.com>
2020-07-02 19:07:58 -0700

    Added styles and minor adjustments

--- static/boggle.js
+++ static/boggle.js
@@ -1,3 +1,4 @@
+//Boggle class which handles game UI functions such as time, score, user word submissions, and displaying information to user
 class Boggle {
 	constructor(gameTimeSecs = 60) {
 		this.gameTimeSecs = gameTimeSecs;
@@ -34,12 +35,12 @@ class Boggle {
 
 	//Handles scoring a given word based on validation result returned by server
 	async scoreWord(word, result) {
-		console.log(word, result);
 		//only score words that are valid and on board, otherwise display why word is not scored
 		const results = {
 			'not-on-board' : () => {
 				$('#result').text(result);
 				this.attemptedWords.add(word);
+				$('#attempted-words').append(document.createTextNode(word.toUpperCase() + ' '));
 			},
 			ok             : () => {
 				//catch if word has been used
@@ -52,14 +53,17 @@ class Boggle {
 				this.updateScore(word);
 				this.displayScore();
 				this.scoredWords.add(word);
+				$('#scored-words').append(document.createTextNode(word.toUpperCase() + ' '));
 			},
 			'not-word'     : () => {
 				$('#result').text(result);
 				this.attemptedWords.add(word);
+				$('#attempted-words').append(document.createTextNode(word.toUpperCase() + ' '));
 			}
 		};
 
 		results[result]();
+		// this.displayWords();
 	}
 
 	//update score based on valid word length
@@ -68,11 +72,16 @@ class Boggle {
 		this.score += wordScore;
 	}
 
-	//display score to user
-	displayScore() {
-		$('#score-container').text(this.score);
+	async displayWords() {
+		//Display scored and attempted words to user as they are entered
 	}
 
+	//display score to user
+	displayScore() {
+		$('#score').text(this.score);
+	}
+
+	//Handles counting down the timer by 1 each second, and handles if time reaches 0 and the game if finished by stopping timer, blocking new words, and calling for updateUserStats.
 	timerCount() {
 		this.gameTimeSecs -= 1;
 		this.displayTimer();
@@ -85,14 +94,30 @@ class Boggle {
 			this.updateUserStats();
 		}
 	}
+	//Display timer to user
 	displayTimer() {
 		$('#timer-container').text(this.gameTimeSecs);
 	}
 
+	//Sends necessary information to update user stats (score and times played) on the server
 	async updateUserStats() {
 		//send submitted end score to server for saving
 		const res = await axios.post('/save-stats', { score: this.score });
-		const result = res.data;
-		console.log(result);
+		const newHighscore = res.data;
+		console.log(newHighscore.new_highscore);
+
+		if (newHighscore.new_highscore) {
+			console.log('new');
+			$('#end-splash').show();
+			$('#end-splash-text').text(
+				`Congratulations! New high score of ${this.score}! Would you like to play again?`
+			);
+		}
+		else {
+			console.log('else');
+			$('#end-splash').show();
+
+			$('#end-splash-text').text(`You scored ${this.score}! Would you like to play again?`);
+		}
 	}
 }
